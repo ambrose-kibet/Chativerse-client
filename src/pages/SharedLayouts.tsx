49a58959be958b308eddf2c0dab446d859f48e71
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-10 22:19:47 +0300

    fix main stlinging max width

--- src/pages/SharedLayouts.tsx
+++ src/pages/SharedLayouts.tsx
@@ -14,7 +14,7 @@ export default SharedLayouts;
 const MainContainer = styled.main`
   width: 100%;
   height: 100%;
-  max-width: var(--max-width);
+  max-width: 1600px;
   margin: 0 auto;
   display: grid;
   grid-template-columns: auto 1fr;
