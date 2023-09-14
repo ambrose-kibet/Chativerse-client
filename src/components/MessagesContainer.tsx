635acb4d1690e37f4d016cd6cab19a01f2589d33
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-14 22:46:31 +0300

    create messages container

---
+++ src/components/MessagesContainer.tsx
@@ -0,0 +1,147 @@
+import styled from 'styled-components';
+import { getMessages } from '../redux/features/chatSlice';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { RootState } from '../redux/store';
+import { useEffect, useRef } from 'react';
+import hello from '../assets/say-hello.gif';
+import moment from 'moment';
+const MessagesContainer = () => {
+  const dispatch = useAppDispatch();
+  const { currentChatId, currentChatMessages } = useAppSelector(
+    (state: RootState) => state.chat
+  );
+  const { user } = useAppSelector((state: RootState) => state.auth);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!currentChatId) return;
+    dispatch(getMessages(currentChatId));
+  }, [currentChatId, dispatch]);
+
+  useEffect(() => {
+    if (!messagesEndRef.current) return;
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, [currentChatMessages]);
+
+  return (
+    <Messages>
+      {(currentChatMessages.length > 0 &&
+        currentChatMessages.map((message) => {
+          return (
+            <div
+              className={user?.userId === message.sender ? 'chat sent' : 'chat'}
+              key={message._id}
+            >
+              {message.imageUrl && <img src={message.imageUrl} alt="image" />}
+              {message.text && <p className="chat-text">{message.text}</p>}
+              <p className="date">{moment(message.createdAt).calendar()}</p>
+            </div>
+          );
+        })) || (
+        <div className="no-messages">
+          <div className="img-container">
+            <img src={hello} alt="hello gif" />
+          </div>
+          <h3>Say hello...</h3>
+        </div>
+      )}
+
+      <div className="last-item" ref={messagesEndRef}></div>
+    </Messages>
+  );
+};
+export default MessagesContainer;
+const Messages = styled.div`
+  height: calc(100vh - 9.5rem);
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 0.25rem;
+  padding: 0 0.5rem;
+
+  h3 {
+    margin: 0;
+    font-size: 0.75rem;
+    color: var(--color-gray);
+    text-align: center;
+    text-transform: capitalize;
+    font-weight: 700;
+  }
+  .chat {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+    align-items: flex-start;
+    width: fit-content;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    background: var(--color-white-100);
+    img {
+      width: 100%;
+      height: auto;
+      border-radius: 0.5rem;
+    }
+    .chat-text {
+      margin: 0;
+      font-size: 0.75rem;
+      color: var(--color-gray);
+      text-align: left;
+      font-weight: 700;
+    }
+
+    &.sent {
+      align-items: flex-end;
+      background: linear-gradient(
+        90deg,
+        var(--color-blue-600),
+        var(--color-blue-400)
+      );
+      color: var(--color-white-100);
+      .chat-text {
+        color: var(--color-white-100);
+      }
+      .date {
+        color: var(--color-white-100);
+      }
+    }
+    .date {
+      margin: 0;
+      font-size: 0.5rem;
+      color: var(--color-gray);
+      text-align: right;
+    }
+  }
+
+  .last-item {
+    width: 100%;
+    height: 0.25rem;
+    visibility: hidden;
+  }
+  .no-messages {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.5rem;
+    h3 {
+      margin: 0;
+      font-size: 1rem;
+      color: var(--color-blue-400);
+      text-align: center;
+      text-transform: capitalize;
+      font-weight: 700;
+    }
+    .img-container {
+      width: 100%;
+      max-width: 300px;
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+  @media screen and (max-width: 768px) {
+    height: calc(100vh - 6rem);
+  }
+`;
