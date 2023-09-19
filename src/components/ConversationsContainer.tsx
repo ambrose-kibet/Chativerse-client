eb710c8b13279e9b89c78e9dfd9c90d754fa37f0
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-19 12:54:00 +0300

    refactor styling

--- src/components/ConversationsContainer.tsx
+++ src/components/ConversationsContainer.tsx
@@ -1,37 +1,14 @@
 import { styled } from 'styled-components';
-import InputComponent from './InputComponent';
-import { BsChevronDown } from 'react-icons/bs';
 import { RootState } from '../redux/store';
-import { useAppDispatch, useAppSelector } from '../redux/hooks';
-import { setSearch } from '../redux/features/chatSlice';
+import { useAppSelector } from '../redux/hooks';
 import ConvoComponent from './ConvoComponent';
 
 const ConversationsContainer = () => {
-  const { searchTerm, conversations } = useAppSelector(
-    (state: RootState) => state.chat
-  );
+  const { conversations } = useAppSelector((state: RootState) => state.chat);
   const { user } = useAppSelector((state: RootState) => state.auth);
-  const dispatch = useAppDispatch();
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearch({ name: e.target.name, value: e.target.value }));
-  };
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+
   return (
     <ConvoContainer>
-      <form onSubmit={handleSubmit}>
-        <InputComponent
-          placeholder="search"
-          value={searchTerm}
-          type="text"
-          name="searchTerm"
-          handleChange={handleChange}
-        />
-        <button type="submit">
-          messages <BsChevronDown />
-        </button>
-      </form>
       <ul className="conversations-list">
         {conversations.map((conversation, index) => (
           <ConvoComponent conversation={conversation} key={index} user={user} />
@@ -47,43 +24,12 @@ const ConvoContainer = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  gap: 0rem;
   width: 100%;
-  height: calc(100vh - 6rem);
-  max-height: calc(100vh - 6rem);
-  overflow-y: scroll;
+  height: 100%;
 
-  form {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    align-items: center;
-    margin-bottom: 1rem;
-    background: var(--color-white-100);
-    input {
-      border: none;
-      margin: 0;
-      padding: 1rem;
-
-      &::placeholder {
-        color: var(--color-gray);
-      }
-    }
-    button {
-      padding: 0.5rem 1rem;
-      border-radius: 0.25rem;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border: none;
-      text-transform: capitalize;
-      text-decoration: none;
-      color: var(--color-gray);
-      background: transparent;
-    }
-  }
   .conversations-list {
     width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     grid-template-columns: 1fr;
@@ -91,7 +37,6 @@ const ConvoContainer = styled.div`
     justify-content: flex-start;
   }
   @media screen and (max-width: 768px) {
-    height: calc(100vh - 3rem);
-    max-height: calc(100vh - 3rem);
+    margin-top: 1rem;
   }
 `;
