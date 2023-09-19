659a6248139727892d8b5cde03030937486fea78
ambrose-kibet <ambrosekibet36@gmail.com>
2023-09-19 12:51:56 +0300

    refactor to show no contacts on empty contacts

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -29,11 +29,17 @@ const ContactsPage = () => {
           dispatch(setSearch({ name: e.target.name, value: e.target.value }));
         }}
       />
-      <div className="contacts-container">
-        {contacts.map((contact) => (
-          <Contact key={contact._id} contact={contact} />
-        ))}
-      </div>
+      {(contacts.length > 0 && (
+        <div className="contacts-container">
+          {contacts.map((contact) => (
+            <Contact key={contact._id} contact={contact} />
+          ))}
+        </div>
+      )) || (
+        <div className="no-contacts">
+          <h3>no contacts</h3>
+        </div>
+      )}
     </ContactsContainer>
   );
 };
@@ -42,21 +48,33 @@ export default ContactsPage;
 const ContactsContainer = styled.section`
   width: 100%;
   height: 100%;
-  display: grid;
-  grid-template-columns: 1fr;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   gap: 1rem;
   padding: 0.5rem;
   max-width: 700px;
   margin: 0 auto 0 0;
-  align-items: flex-start;
+
   input {
     width: 100%;
     background: var(--color-white-100);
     border: none;
     height: fit-content;
   }
+  .no-contacts {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    h3 {
+      margin: 0;
+      font-size: 1rem;
+      color: var(--color-gray);
+    }
+  }
+
   .contacts-container {
-    height: calc(100vh - 6rem);
     overflow-y: scroll;
     display: flex;
     flex-direction: column;
